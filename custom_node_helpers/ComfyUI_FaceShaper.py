61a40085cc9df7d48141fb26ab74116e6ce5630e
xiaozhou <332506119@qq.com>
2025-02-21 09:20:52 +0800

    ewew

--- custom_node_helpers/ComfyUI_FaceShaper.py
+++ custom_node_helpers/ComfyUI_FaceShaper.py
@@ -1,15 +1,13 @@
 from custom_node_helper import CustomNodeHelper
 
+MODELS = [
+    "landmark.onnx",
+]
 class ComfyUI_FaceShaper(CustomNodeHelper):
 
     @staticmethod
     def add_weights(weights_to_download, node):
         if node.is_type("FaceShaperCropper"):
-            from weights_downloader import WeightsDownloader
-            weights_downloader = WeightsDownloader()
-            weights_downloader.download_if_not_exists(
-                "landmark.onnx",
-                "https://huggingface.co/redstarxz/comfyw/resolve/main/landmark.onnx.tar",
-                "ComfyUI/models/liveportrait/",
-            )
+            weights_to_download.extend(MODELS)
+
 
